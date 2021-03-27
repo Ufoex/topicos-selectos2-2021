a1401a77971aa05247a10d0076643e9778ed971e
Ufoex <masterllamas@hotmail.com>
2021-03-26 22:50:20 -0600

    Primer

---
+++ app/Http/Controllers/AltaProductoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AltaProductoController extends Controller
+{
+    public function index(){
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\AcercaController;
+use App\Http\Controllers\AltaProductoController;
+use App\Http\Controllers\DetalleProductoController;
+use App\Http\Controllers\DetalleProveedor;
+use App\Http\Controllers\DetalleUsuarioController;
+use App\Http\Controllers\RegistroProveedor;
+use App\Http\Controllers\RegistroUsuarioController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard/registroUsuario',[RegistroUsuarioController::class, 'index']);
+Route::get('/dashboard/detalleUsuario',[DetalleUsuarioController::class, 'index'])->name('Usuario');
+Route::get('/dashboard/altaProducto',[AltaProductoController::class, 'index']);
+Route::get('/dashboard/detalleProducto',[DetalleProductoController::class, 'index'])->name('Producto');
+Route::get('/dashboard/detalleProveedor',[DetalleProveedor::class,'index'])->name('Proveedor');
+Route::get('/dashboard/altaProveedor',[RegistroProveedor::class,'index']);
+Route::get('/dashboard/acerca',[AcercaController::class, 'index'])->name('Acerca');
